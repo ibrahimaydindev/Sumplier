1db7aceac3d56c9d3ca8b58f588d9fa5320db2fe
ibrahimaydin <55055268+ibrahimaydindev@users.noreply.github.com>
2025-01-07 22:42:12 +0300

    Sumplier
    - App updated.

---
+++ app/src/main/java/com/sumplier/app/data/Category.kt
@@ -0,0 +1,9 @@
+package com.sumplier.app.data
+
+data class Category(
+    private val id: Int?,
+    private val categoryCode: String?,
+    private val categoryName: String?,
+    private val isActive: Boolean?,
+    private val companyCode: String?
+)
---
+++ app/src/main/java/com/sumplier/app/data/TicketOrder.kt
@@ -0,0 +1,5 @@
+package com.sumplier.app.data
+
+data class TicketOrder (
+    
+)
