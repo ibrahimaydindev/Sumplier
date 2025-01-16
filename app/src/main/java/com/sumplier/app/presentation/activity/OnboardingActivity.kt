fb730050772753f24a123346e890db9ff9912981
serdarrahmet <ahmetserdarkarabulut@gmail.com>
2025-01-17 00:44:37 +0300

    LOGIN LAYOUTS ADDED

--- app/src/main/java/com/sumplier/app/presentation/activity/OnboardingActivity.kt
+++ app/src/main/java/com/sumplier/app/presentation/activity/OnboardingActivity.kt
@@ -3,64 +3,72 @@ package com.sumplier.app.presentation.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.ViewFlipper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.textfield.TextInputEditText
 import com.sumplier.app.R
-import com.sumplier.app.app.Config
 import com.sumplier.app.data.enums.ConfigState
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 internal enum class OnboardingPages {
     PROGRESS,
-    LOGIN,
+    COMPANY_LOGIN,
+    USER_LOGIN,
     RESET_PASSWORD_EMAIL,
     RESET_PASSWORD_CODE,
     NEW_PASSWORD,
-    SUCCESS,
-    FAIL
+
 }
 
 class OnboardingActivity : AppCompatActivity() {
 
-    private var currentState = ConfigState.USER
+    private var currentState = ConfigState.COMPANY
     private lateinit var viewFlipper: ViewFlipper
-    private lateinit var btnLogin: Button
-    
+    private lateinit var btnCompanyLogin: ConstraintLayout
+    private lateinit var btnUserLogin: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.e("Onboarding", "onCreate")
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_onboarding)
-        
+
         viewFlipper = findViewById(R.id.viewFlipper)
-        setUpLoginScreen();
+        setUpCompanyLoginScreen()
+        setUpUserLoginScreen()
         setupViews()
-        
+
         processCurrentState()
-        //setView(OnboardingPages.PROGRESS);
+
 
     }
-    
 
-    private fun setUpLoginScreen(){
-        btnLogin = findViewById<Button>(R.id.btnLogin)
-        btnLogin.setOnClickListener {
+
+    private fun setUpCompanyLoginScreen() {
+
+        val loginButtonText: TextView = findViewById(R.id.companyLogin_button_text)
+
+
+        btnCompanyLogin = findViewById(R.id.btnCompanyLogin)
+        btnCompanyLogin.setOnClickListener {
             val email = findViewById<TextInputEditText>(R.id.etEmail)?.text.toString()
             val password = findViewById<TextInputEditText>(R.id.etPassword)?.text.toString()
-            
+
             when {
-                email.isEmpty() -> showLoginError("Lütfen email adresinizi giriniz")
-                password.isEmpty() -> showLoginError("Lütfen şifrenizi giriniz")
                 else -> {
-                    btnLogin.isEnabled = false
-                    btnLogin.text = "Giriş yapılıyor..."
-                    fetchUser(email, password, 1)
+                    btnCompanyLogin.isEnabled = false
+                    loginButtonText.text = "Giriş yapılıyor..."
+                    fetchCompany(email, password, 1)
                 }
             }
         }
@@ -70,72 +78,69 @@ class OnboardingActivity : AppCompatActivity() {
         }
 
     }
-    private fun setupViews() {
 
+    private fun setUpUserLoginScreen() {
+
+        val loginButtonText: TextView = findViewById(R.id.userLogin_button_text)
+
+
+        btnUserLogin = findViewById(R.id.btnLoginUser)
+        btnUserLogin.setOnClickListener {
+            val email = findViewById<TextInputEditText>(R.id.etEmail)?.text.toString()
+            val password = findViewById<TextInputEditText>(R.id.etPassword)?.text.toString()
+
+            when {
+                else -> {
+                    btnUserLogin.isEnabled = false
+                    loginButtonText.text = "Giriş yapılıyor..."
+                    fetchCompany(email, password, 1)
+                }
+            }
+        }
+
+        findViewById<TextView>(R.id.tvForgotPasswordUser)?.setOnClickListener {
+            setView(OnboardingPages.RESET_PASSWORD_EMAIL)
+        }
+
+    }
+
+    private fun setupViews() {
 
         // Reset Password Email View
         findViewById<Button>(R.id.btnSendCode)?.setOnClickListener {
             val email = findViewById<TextInputEditText>(R.id.etEmail)?.text.toString()
             if (email.isNotEmpty()) {
-                // Email'e kod gönderme işlemi
                 setView(OnboardingPages.RESET_PASSWORD_CODE)
             }
         }
 
         findViewById<Button>(R.id.btnBack)?.setOnClickListener {
-            setView(OnboardingPages.LOGIN)
+            setView(OnboardingPages.USER_LOGIN)
         }
 
-        // Reset Password Code View
-        findViewById<Button>(R.id.btnVerifyCode)?.setOnClickListener {
-            val code = findViewById<TextInputEditText>(R.id.etCode)?.text.toString()
-            if (code.length == 6) {
-                // Kod doğrulama işlemi
-                setView(OnboardingPages.NEW_PASSWORD)
-            }
-        }
 
-        findViewById<TextView>(R.id.tvResendCode)?.setOnClickListener {
-            // Kodu tekrar gönderme işlemi
-        }
-
-        // New Password View
-        findViewById<Button>(R.id.btnSavePassword)?.setOnClickListener {
-            val newPassword = findViewById<TextInputEditText>(R.id.etNewPassword)?.text.toString()
-            val confirmPassword = findViewById<TextInputEditText>(R.id.etConfirmPassword)?.text.toString()
-            
-            when {
-                newPassword.isEmpty() -> showFailView("Lütfen yeni şifrenizi girin")
-                confirmPassword.isEmpty() -> showFailView("Lütfen şifrenizi tekrar girin")
-                newPassword != confirmPassword -> showFailView("Şifreler eşleşmiyor")
-                else -> {
-                    // Şifre değiştirme API çağrısı
-                    // Başarılı olursa:
-                    setView(OnboardingPages.SUCCESS)
-                    // Başarısız olursa:
-                    // showFailView("Şifre değiştirme işlemi başarısız oldu")
-                }
-            }
-        }
     }
-    
+
     private fun setView(page: OnboardingPages) {
         viewFlipper.displayedChild = when (page) {
-            OnboardingPages.PROGRESS -> 0
-            OnboardingPages.LOGIN -> 1
-            OnboardingPages.RESET_PASSWORD_EMAIL -> 2
-            OnboardingPages.RESET_PASSWORD_CODE -> 3
-            OnboardingPages.NEW_PASSWORD -> 4
-            OnboardingPages.SUCCESS -> 5
-            OnboardingPages.FAIL -> 6
+            OnboardingPages.COMPANY_LOGIN -> 0
+            OnboardingPages.PROGRESS -> 1
+            OnboardingPages.USER_LOGIN -> 2
+            OnboardingPages.RESET_PASSWORD_EMAIL -> 3
+            OnboardingPages.RESET_PASSWORD_CODE -> 4
+            OnboardingPages.NEW_PASSWORD -> 5
+
         }
     }
-    
+
     private fun processCurrentState() {
         lifecycleScope.launch {
             try {
+                Log.e("Onboarding", "state: $currentState")
+
                 when (currentState) {
-                    ConfigState.USER -> checkSetUser()
+                    ConfigState.COMPANY -> checkSetCompany()
+                    ConfigState.USER -> userLogin()
                     //ConfigState.PRODUCTS -> fetchProductData()
                     ConfigState.COMPLETED -> navigateToMain()
                     else -> Log.e("Onboarding", "Bilinmeyen state: $currentState")
@@ -145,69 +150,57 @@ class OnboardingActivity : AppCompatActivity() {
             }
         }
     }
-    
-    private fun checkSetUser() {
 
-        if (Config.getInstance().getCurrentUser() != null){
+    private suspend fun checkSetCompany() {
 
-            currentState = ConfigState.COMPLETED
-            showToastMessage("Giriş başarılı")
-            processCurrentState()
-            return
-        }else
-            setView(OnboardingPages.LOGIN)
+        setView(OnboardingPages.PROGRESS)
+        delay(4000)
+        setView(OnboardingPages.COMPANY_LOGIN)
+        delay(4000)
+
+        currentState = ConfigState.USER
+        processCurrentState()
+
 
     }
 
-    private fun fetchUser(email: String, password: String, loginType: Int) {
-        
+    private suspend fun userLogin() {
+
+        setView(OnboardingPages.PROGRESS)
+        delay(4000)
+
+        setView(OnboardingPages.USER_LOGIN)
+        delay(4000)
+
+        currentState = ConfigState.COMPLETED
+        processCurrentState()
     }
-    
-    //private suspend fun fetchProductData() {
-    //    Log.d("Onboarding", "Ürün bilgileri çekiliyor...")
-    //    val productResponse = ApiService.getProductState()
-    //
-    //    if (productResponse.isSuccessful) {
-    //        Log.d("Onboarding", "Ürün bilgileri başarıyla alındı")
-    //        DataStorage.saveData(ConfigKey.PRODUCTS, productResponse.data)
-    //
-    //        // Tüm işlemler tamamlandı
-    //        currentState = ConfigState.COMPLETED
-    //        processCurrentState()
-    //    } else {
-    //        Log.e("Onboarding", "Ürün bilgileri alınamadı: ${productResponse.error}")
-    //        handleError("Ürün bilgileri alınamadı")
-    //    }
-    //}
-    
-    private fun navigateToMain() {
+
+    private fun fetchCompany(email: String, password: String, loginType: Int) {
+
+    }
+
+
+    private suspend fun navigateToMain() {
+
+        delay(4000)
+
         Log.d("Onboarding", "Tüm veriler başarıyla yüklendi")
         val intent = Intent(this, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
         finish()
     }
-    
+
     private fun showFailView(errorMessage: String) {
-        setView(OnboardingPages.FAIL)
+
         findViewById<TextView>(R.id.tvFailMessage)?.text = errorMessage
-        
+
         findViewById<Button>(R.id.btnRetry)?.setOnClickListener {
-            setView(OnboardingPages.LOGIN) // veya başka bir sayfaya yönlendirme
+            setView(OnboardingPages.USER_LOGIN) // veya başka bir sayfaya yönlendirme
         }
-    }
-    
-    private fun handleError(message: String) {
-        showFailView(message)
     }
 
-    private fun showLoginError(message: String) {
-        val tvLoginError = findViewById<TextView>(R.id.tvLoginError)
-        tvLoginError?.apply {
-            text = message
-            visibility = View.VISIBLE
-        }
-    }
 
     private fun showToastMessage(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
